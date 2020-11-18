46e739c2ce2abb55736e230ab667f55de833dfa8
vovakhrom <71398339+vovakhrom@users.noreply.github.com>
2020-11-18 17:49:17 +0300

    Add files via upload

--- Node.java
+++ Node.java
@@ -1,18 +1,22 @@
-public class Node {
+package siaod1lab;
 
-	    public int data;                    //Хранимые данные (ключ)
-	    public boolean leftIsThread;        //Истина, если ссылка на левое поддерево ЯВЛЯЕТСЯ нитью
+
+
+public class Node {
+	
+	    public int data;                    
+	    public boolean lIsNotThread;        //Истина, если ссылка на левое поддерево НЕ является нитью
+	    public boolean rIsNotThread;        //Истина, если ссылка на правое поддерево НЕ является нитью
 	    public Node left;                   //Ссылка на левое поддерево
 	    public Node right;                  //Ссылка на правое поддерево
 
 	    public Node(int data){              //Конструтор для инициализации
 	        this.data = data;
-	        leftIsThread = false;
+	        lIsNotThread = true;
+	        rIsNotThread = true;
 	        left  = null;
 	        right = null;
 	    }
-
-	    //Метод для вывода ключа
 	    public void show() { System.out.print(data + " "); }
 	
 }
